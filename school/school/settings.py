8f41aafea574ab5d3fe8a683c7a8bb547301b608
Shahadsha <shahadshaazz+git@gmail.com>
2022-05-21 14:27:09 +0530

    ready to deploy heroku

--- school/school/settings.py
+++ school/school/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qi+h-j27w+2j$piaq-qtp^1y$!pynl(1mrc_+*#xa=kn@ljd(2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
